5c81298f7f8b1734df011df64e12e945a39074a7
spazerzex <antriksh5goel@gmail.com>
2019-09-03 20:51:59 +0530

    first

--- app/src/main/java/com/example/myapplication1/registrationactivity.java
+++ app/src/main/java/com/example/myapplication1/registrationactivity.java
@@ -2,10 +2,13 @@ package com.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class registrationactivity extends AppCompatActivity {
     private EditText username,userpassword,useremail;
@@ -15,8 +18,27 @@ public class registrationactivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registrationactivity);
-    private void setupuiviews(){
+        setContentViews(R.layout.activity_registrationactivity);
+        setup();
+    regButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if(validate()){
+
+            }
+        }
+    });
+    userlogin.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+startActivity(new Intent(registrationactivity.this,MainActivity.class));
+        }
+    });}
+
+    private void setContentViews(int activity_registrationactivity) {
+    }
+
+    private void setup(){
         username =(EditText)findViewById(R.id.etusername);
         userpassword = (EditText)findViewById(R.id.etpassword);
         useremail = (EditText)findViewById(R.id.etuseremail);
@@ -30,5 +52,18 @@ public class registrationactivity extends AppCompatActivity {
 
 
 
-        }}
+        }
+        private boolean validate(){
+        boolean result = false;
+        String name = username.getText().toString();
+            String password = userpassword.getText().toString();
+            String email = useremail.getText().toString();
+
+    if(name.isEmpty() && password.isEmpty() && email.isEmpty()){
+        Toast.makeText(this,"Please Enter The Details",Toast.LENGTH_SHORT);
+    }else{
+        result = true;
+    }
+    return result;
+        }
 }
